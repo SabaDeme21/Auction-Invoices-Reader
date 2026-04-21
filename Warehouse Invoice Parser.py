cd46e50682b4653eda5906e7938aa19a0ff9b855
SabaDeme21 <160774792+SabaDeme21@users.noreply.github.com>
2026-04-21 23:05:47 +0400

    Update Warehouse Invoice Parser.py

--- Warehouse Invoice Parser.py
+++ Warehouse Invoice Parser.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import gspread
 import logging
 import warnings
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
 
 # ==========================
 # LOGGING SETUP
@@ -43,6 +44,10 @@ def detect_company(text):
         return 'INDIGO [AUTOMOBY]'
     elif re.search(r"INDIGOCARS GE LLC", text.upper(), re.IGNORECASE):
         return 'INDIGO [GEORGIA]'
+    elif re.search(r"PAZA MOTORS", text.upper(), re.IGNORECASE):
+        return 'PAZA MOTORS'
+    elif re.search(r'TRT', text.upper(), re.IGNORECASE):
+        return 'TRT'
     return None
 
 def find_atlantic_invoice_table(tables):
@@ -195,8 +200,6 @@ def atlantic_parser(text, table, company, filename):
     else:
         price_df = pd.concat(price_dfs, ignore_index=True)
 
-    print(price_df)
-
     price_df['Container Number'] = container_number
     price_df['Company'] = 'Atlantic'
     price_df['Invoice Date'] = df.at[0, 'Date']
@@ -263,13 +266,14 @@ def w8_america_parser(text, table, company, filename):
 
     df = pd.DataFrame(flat_rows, columns=header)
     df = df.loc[(df['Description'].notna())&(df['Description']!='')]
-    vins = [v[1:] for v in re.findall(r'#[A-HJ-NPR-Z0-9]{17}', text)]
+    vins = [v[1:] for v in re.findall(r'#[A-HJ-NPR-Z0-9]{6,17}', text)]
     df['VIN'] = None
 
     for i in range(len(vins)):
         df.loc[i, 'VIN'] = vins[i]
 
     df = df[['VIN', 'Other', 'Delivery', 'Shipping', 'Storage', 'EVfee', 'Add.Services']]
+    
     df = df.loc[df['VIN'].notna()]
     df = df.melt(
     id_vars='VIN',
@@ -289,7 +293,7 @@ def w8_america_parser(text, table, company, filename):
 
     df = df[['Company', 'Invoice Date', 'Due Date', 'Invoice Number',
         'Container Number', 'VIN', 'Description of Charges', 'SST', 'შენიშვნა', 'Amount']]
-    
+        
     df = df.assign(Full_Amount = df.groupby('VIN')['Amount'].transform('sum'))
 
     return df
@@ -351,7 +355,163 @@ def indigo_parser(text, table, company, filename):
     df = df[['Company', 'Invoice Date', 'Due Date', 'Invoice Number', 'Container Number', 
              'VIN', 'Description of Charges', 'SST', 'შენიშვნა', 'Amount', 'Full_Amount']]
 
+    return df
+
+def paza_parser(text, table, company, filename):
+    def date_table(tables):
+        required = {'DATE', 'INVOICE #'}
+        match_table = []
+
+        for tabl in tables:
+            if not tabl or not tabl[0]:
+                continue
+
+            header = [c.replace('\n', '').strip().upper() for c in tabl[0] if c]
+            if required.issubset(set(header)):
+                match_table.append(tabl)
+            
+        if not match_table:
+            raise ValueError("Paza date table not found")
+
+        match_table_df = pd.DataFrame(match_table[0][1:], columns=match_table[0][0])
+        return match_table_df
+    
+    def container_table(tables):
+        required = {'SAILING DATE', 'ARRIVAL DATE', 'BOOKING#', 'CONTAINER#'}
+        match_table = []
+
+        for tabl in tables:
+            if not tabl or not tabl[0]:
+                continue
+            
+            header = [c.replace('\n', '').strip().upper() for c in tabl[0] if c]
+            
+            if required.issubset(set(header)):
+                match_table.append(tabl)
+
+        if not match_table:
+            raise ValueError("Paza container table not found")
+        
+        cont_table = match_table[0][0] 
+        cont_values = match_table[0][1]
+        containers = pd.DataFrame([cont_values], columns=cont_table)
+
+        car_table = match_table[0][2]
+        car_table_list = [c for c in car_table if c is not None]
+
+        car_values = match_table[0][3:]
+        car_values_list = []
+        for v in car_values:
+            list_values = []
+            for val in v:
+                if val is not None and val != '':
+                    list_values.append(val)
+            car_values_list.append(list_values)
+
+        car_desc = [li for li in car_values_list if len(li) == 3]
+
+        car_info = pd.DataFrame(car_desc, columns=car_table_list)
+
+        return containers, car_info
+    
+    date_df = date_table(table)
+    container_df = container_table(table)[0]
+    car_df = container_table(table)[1]
+    car_df['Amount'] = car_df['Amount'].str.replace(',', '').astype(float)
+    
+    cont_date = date_df['Date'].apply(time_transform).iloc[0]
+    due_date = date_df['Date'].apply(time_transform).iloc[0]
+    invoice_number = date_df['Invoice #'].iloc[0]
+    container_number = container_df['Container#'].iloc[0]
+    desc_info = car_df.loc[car_df['Amount'] > 0].copy()
+    car_info = car_df.loc[car_df['Amount'] == 0].copy()
+    car_info['VIN'] = car_info['Description'].str.extract(r'\b([A-HJ-NPR-Z0-9]{17})\b')
+    car_info = car_info[['VIN']]
+    desc_info['Each VIN'] = desc_info['Amount'] / len(car_info)
+    desc_info = desc_info[['Description', 'Each VIN']]
+
+    car_info['Company'] = company.lower().title()
+    car_info['Invoice Date'] = cont_date
+    car_info['Due Date'] = due_date
+    car_info['Invoice Number'] = invoice_number
+    car_info['Container Number'] = container_number
+    car_info['SST'] = ''
+    car_info['შენიშვნა'] = ''
+    car_info['Full_Amount'] = desc_info['Each VIN'].iloc[0] if len(desc_info) == 1 else desc_info['Each VIN'].iloc[0] * len(car_info)
+
+    if len(desc_info) == 1:
+        car_info = car_info.assign(
+            **{
+                'Description of Charges': desc_info['Description'].iloc[0],
+                'Amount': desc_info['Each VIN'].iloc[0]
+            }
+        )
+    else:
+        # Duplicate car_info for every description
+        car_info = pd.concat([car_info] * len(desc_info), ignore_index=True)
+        
+        # Create repeated descriptions and amounts
+        repeated_desc = desc_info.loc[desc_info.index.repeat(len(car_info) // len(desc_info))].reset_index(drop=True)
+        
+        car_info['Description of Charges'] = repeated_desc['Description'].values
+        car_info['Amount'] = repeated_desc['Each VIN'].values
+
+    car_info['Description of Charges'] = car_info['Description of Charges'].str.replace('\n', ' ').str.strip()
+
+    df = car_info[['Company', 'Invoice Date', 'Due Date', 'Invoice Number', 'Container Number', 'VIN', 'Description of Charges', 'SST', 'შენიშვნა', 'Amount', 'Full_Amount']].copy()
+
     print(df)
+
+    return df
+
+def trt_parser(text, table, company, filename):
+    vin_pattern = re.findall(r'\b[A-HJ-NPR-Z0-9]{17}\b', text.upper(), re.IGNORECASE)
+    if vin_pattern:
+        vin = vin_pattern
+    
+    vin = list(set(vin))
+
+    price_pattern = re.search(r'[TRANSPORTATION, T RANSPORTATION]\s*\$\s*([\d,\.]+)', text.upper(), re.IGNORECASE)
+    if price_pattern:
+        price = price_pattern.group(0)
+        if 'N' in price:
+            price = price.replace('N', '')
+        if ' ' in price:
+            price = price.replace(' ', '')
+        if '$' in price:
+            price = price.replace('$', '')
+        if ',' in price:
+            price = price.replace(',', '.')
+
+    container_pattern = re.search(r'\b[A-Z]{4}\d{7}\b', text.upper(), re.IGNORECASE)
+    if container_pattern:
+        container = container_pattern.group(0)
+
+    date_pattern = re.search(r'\b(\d{1,2}/\d{1,2}/\d{4})\s+(\d{6,})\b', text.upper(), re.IGNORECASE)
+    if date_pattern:
+        date = time_transform(date_pattern.group(1))
+        invoice_number = date_pattern.group(2)
+
+    price = float(price) / len(vin)
+
+    final_df = []
+
+    for v in vin:
+        final_df.append({
+            'Company': company,
+            'Invoice Date': date,
+            'Due Date': date,
+            'Invoice Number': invoice_number,
+            'Container Number': container,
+            'VIN': v,
+            'Description of Charges': 'Transportation',
+            'SST': '',
+            'შენიშვნა': '',
+            'Amount': price,
+            'Full_Amount': price
+        })
+    
+    df = pd.DataFrame(final_df)                                          
 
     return df
 
@@ -362,10 +522,10 @@ def google_sheet_updater(df):
 
     sh.clear()
     data = df[['Company', 'Invoice Date', 'Due Date', 'Invoice Number', 'Container Number', 
-                   'VIN', 'Description of Charges', 'Amount', 'Full_Amount']].values.tolist()
+                   'VIN', 'Description of Charges', 'SST', 'შენიშვნა', 'Amount', 'Full_Amount']].values.tolist()
     columns = ['Company', 'Invoice Date', 'Due Date', 'Invoice Number', 'Container Number', 
-                   'VIN', 'Description of Charges', 'Amount', 'Full_Amount']
-    sh.update([columns] + data)
+                   'VIN', 'Description of Charges', 'SST', 'შენიშვნა', 'Amount', 'Full_Amount']
+    set_with_dataframe(sh, pd.DataFrame(data, columns=columns), include_index=False, include_column_header=True)
 
 # ==========================
 # MAIN PROCESS
@@ -375,7 +535,9 @@ parsers = {
     'ATLANTIC': atlantic_parser,
     'W8 SHIPPING': w8_america_parser,
     'INDIGO [AUTOMOBY]': indigo_parser,
-    'INDIGO [GEORGIA]': indigo_parser
+    'INDIGO [GEORGIA]': indigo_parser,
+    'PAZA MOTORS': paza_parser,
+    'TRT': trt_parser
 }
 
 pdf_files = [f for f in os.listdir(FOLDER_PATH) if f.lower().endswith('.pdf')]
@@ -409,13 +571,17 @@ for idx, filename in enumerate(pdf_files, 1):
                 all_tables.extend(page_tables)
 
         # Merge all text into a single string
+
         full_text = "\n".join(all_text)
+
         company = detect_company(full_text)
         if not company:
             raise ValueError("Company not detected")
 
         parser = parsers[company]
         df = parser(full_text, all_tables, company, filename)
+
+        print(df)
         df['Source File'] = filename
         all_dfs.append(df)
 
